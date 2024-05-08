0223eff437fd7e48dcf262778ab0db29330437e1
Andrei Zola <and.zola.77@gmail.com>
2024-05-08 17:29:56 +0200

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("And another people");
+        Student student = new Student();
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class Student implements Studyable{
+}
